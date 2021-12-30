0b425472863a6b8a4373ddfef5bb1be000c5dd77
Alex Castro <alex@fruitbat.org>
2021-12-30 13:10:40 -0800

    Fix errors with extended difficulties in ScreenSelectMusic.
    It'll do nothing if it tries to use the new difficulties, but that's
    fine.

--- Graphics/ScreenSelectMusic DifficultyDisplay/default.lua
+++ Graphics/ScreenSelectMusic DifficultyDisplay/default.lua
@@ -27,65 +27,67 @@ local function GetEdits( in_Song, in_StepsType )
 	end
 end
 --
+local tLocation = {
+	Beginner	= 32*-1.25,
+	Easy 		= 32*-0.25,
+	Medium		= 32*0.75,
+	Hard		= 32*1.75,
+	Challenge	= 32*2.75,
+	Edit 		= 32*4.75,
+}
 for idx,diff in pairs(Difficulty) do
 	local sDifficulty = ToEnumShortString( diff )
-	local tLocation = {
-		Beginner	= 32*-1.25,
-		Easy 		= 32*-0.25,
-		Medium		= 32*0.75,
-		Hard		= 32*1.75,
-		Challenge	= 32*2.75,
-		Edit 		= 32*4.75,
-	}
-	t[#t+1] = Def.ActorFrame {
-		SetCommand=function(self)
-			local c = self:GetChildren()
--- 			local Bar = self:GetChild("Bar")
--- 			local Meter = self:GetChild("Meter"
-			local song = GAMESTATE:GetCurrentSong()
-			local bHasStepsTypeAndDifficulty = false
-			local meter = ""
-			if song then
-				local st = GAMESTATE:GetCurrentStyle():GetStepsType()
-				bHasStepsTypeAndDifficulty = song:HasStepsTypeAndDifficulty( st, diff )
-				local steps = song:GetOneSteps( st, diff )
-				if steps then
-					meter = steps:GetMeter()
-					append = ""
-					--
-					if diff == 'Difficulty_Edit' then
-						meter = GetEdits( song, st )
-						append = ( GetEdits( song, st ) > 1 ) and "Edits" or "Edit"
-						meter = meter .. " " .. append
+	if tLocation[sDifficulty] then
+		t[#t+1] = Def.ActorFrame {
+			SetCommand=function(self)
+				local c = self:GetChildren()
+-- 				local Bar = self:GetChild("Bar")
+-- 				local Meter = self:GetChild("Meter"
+				local song = GAMESTATE:GetCurrentSong()
+				local bHasStepsTypeAndDifficulty = false
+				local meter = ""
+				if song then
+					local st = GAMESTATE:GetCurrentStyle():GetStepsType()
+					bHasStepsTypeAndDifficulty = song:HasStepsTypeAndDifficulty( st, diff )
+					local steps = song:GetOneSteps( st, diff )
+					if steps then
+						meter = steps:GetMeter()
+						append = ""
+						--
+						if diff == 'Difficulty_Edit' then
+							meter = GetEdits( song, st )
+							append = ( GetEdits( song, st ) > 1 ) and "Edits" or "Edit"
+							meter = meter .. " " .. append
+						end
 					end
 				end
-			end
-			c.Meter:settext( meter )
-			self:playcommand( bHasStepsTypeAndDifficulty and "Show" or "Hide" )
-		end,
-		CurrentSongChangedMessageCommand=function(self) self:playcommand("Set") end,
-		--
-		Def.Sprite{
-			Texture= THEME:GetPathG("ScreenSelectMusic","DifficultyDisplay/_barpeice " .. sDifficulty),
-			Name="BarPeice",
-			ShowCommand=function(self)
-				self:stoptweening():linear(0.1):diffuse(CustomDifficultyToColor( sDifficulty )) end,
-			HideCommand=function(self)
-				self:stoptweening():decelerate(0.05):diffuse(CustomDifficultyToDarkColor( sDifficulty )) end,
-			InitCommand=function(self) self:diffuse(CustomDifficultyToColor( sDifficulty )) end,
-		},
-		Def.BitmapText{
-			Font= THEME:GetPathF("StepsDisplay","Meter"),
-			Name="Meter",
-			Text=(sDifficulty == "Edit") and "0 Edits" or "0",
-			ShowCommand=function(self)
-				self:stoptweening():linear(0.1):diffuse(CustomDifficultyToColor( sDifficulty )) end,
-			HideCommand=function(self)
-				self:stoptweening():decelerate(0.05):diffuse(CustomDifficultyToDarkColor( sDifficulty )) end,
-			InitCommand=function(self)
-				self:x(-64-8+tLocation[sDifficulty]):shadowlength(1):zoom(0.75)
-				:diffuse(CustomDifficultyToColor( sDifficulty )) end,
+				c.Meter:settext( meter )
+				self:playcommand( bHasStepsTypeAndDifficulty and "Show" or "Hide" )
+			end,
+			CurrentSongChangedMessageCommand=function(self) self:playcommand("Set") end,
+			--
+			Def.Sprite{
+				Texture= THEME:GetPathG("ScreenSelectMusic","DifficultyDisplay/_barpeice " .. sDifficulty),
+				Name="BarPeice",
+				ShowCommand=function(self)
+					self:stoptweening():linear(0.1):diffuse(CustomDifficultyToColor( sDifficulty )) end,
+				HideCommand=function(self)
+					self:stoptweening():decelerate(0.05):diffuse(CustomDifficultyToDarkColor( sDifficulty )) end,
+				InitCommand=function(self) self:diffuse(CustomDifficultyToColor( sDifficulty )) end,
+			},
+			Def.BitmapText{
+				Font= THEME:GetPathF("StepsDisplay","Meter"),
+				Name="Meter",
+				Text=(sDifficulty == "Edit") and "0 Edits" or "0",
+				ShowCommand=function(self)
+					self:stoptweening():linear(0.1):diffuse(CustomDifficultyToColor( sDifficulty )) end,
+				HideCommand=function(self)
+					self:stoptweening():decelerate(0.05):diffuse(CustomDifficultyToDarkColor( sDifficulty )) end,
+				InitCommand=function(self)
+					self:x(-64-8+tLocation[sDifficulty]):shadowlength(1):zoom(0.75)
+					:diffuse(CustomDifficultyToColor( sDifficulty )) end,
+			}
 		}
-	}
+	end
 end
 return t
